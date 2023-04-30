a5ee9c1c3395e62f4d6d95667362361aa69b2e15
robnsiov <robnsiov@gmail.com>
2023-04-30 22:04:45 +0430

    remove gallery page

--- components/user/pages/gallery/gallery.tsx
+++ components/user/pages/gallery/gallery.tsx
@@ -1,59 +1,6 @@
 // https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg
 
 const Gallery = () => {
-  return (
-    <>
-      <div className="w-full h-full overflow-y-auto grid  md:grid-cols-2 xl:grid-cols-4  grid-cols-6 gap-4">
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className=""
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className="row-span-[1/3]"
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className=""
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className="row-span-[1/3]"
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className=""
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className="row-span-[1/3]"
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className=""
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className="row-span-[1/3]"
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className=""
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className="row-span-[1/3]"
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className=""
-        />
-        <img
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-          className="row-span-[1/3]"
-        />
-      </div>
-    </>
-  );
+  return <></>;
 };
 export default Gallery;
