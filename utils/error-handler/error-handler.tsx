aafe3d8961b9de61924e6d3d3eb6fbab1f4cfe51
robnsiov <robnsiov@gmail.com>
2023-05-19 16:09:45 +0330

    remove console.log :)

--- utils/error-handler/error-handler.tsx
+++ utils/error-handler/error-handler.tsx
@@ -1,7 +1,6 @@
 import { AxiosError } from "axios";
 
 const ErrorHandler = (error: AxiosError | unknown, path: string) => {
-  console.log(error);
   // network errors
   // server errors
   // custom errors
