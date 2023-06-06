46ed798b9e3adb8650dfc0d990c02557e29e4209
robnsiov <robnsiov@gmail.com>
2023-06-06 18:07:48 +0330

    fix axios instance

--- utils/axios/axios.tsx
+++ utils/axios/axios.tsx
@@ -3,9 +3,11 @@ import Axios, { AxiosRequestConfig } from "axios";
 
 function request<T>(config: AxiosRequestConfig) {
   let token = "";
-  if (typeof window !== "undefined")
-    token = localStorage.getItem("token") as string;
-  Axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const tk = localStorage.getItem("token");
+    if (tk) token = tk;
+  }
+  if (token) Axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   const axs = Axios<T>({
     baseURL: api.baseURL,
     ...config,
