7a6d2981fe20ab80deaee9c4abe25516c9e44742
robnsiov <robnsiov@gmail.com>
2023-06-08 22:01:29 +0330

    add statics

--- components/share/container/designed-by/footer.tsx
+++ components/share/container/designed-by/footer.tsx
@@ -1,9 +1,10 @@
 import Link from "../../link/link";
-import { FaTelegramPlane } from "react-icons/fa";
+import { FaLinkedin, FaTelegramPlane } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { BsTwitter } from "react-icons/bs";
 import cx from "classnames";
 import { Box1 } from "iconsax-react";
+import statics from "@/constants/app";
 
 const Footer = () => {
   return (
@@ -16,24 +17,27 @@ const Footer = () => {
           <span className="ml-5 text-sm text-slate-300">
             به ما ملحق شوید در
           </span>
-          <Link href={"/"}>
+          <Link href={statics.linkedinSocial}>
+            <FaLinkedin className="ml-3 hover:text-slate-600" />
+          </Link>
+          <Link href={statics.telegramSocial}>
             <FaTelegramPlane className="hover:text-slate-600" />
           </Link>
-          <Link href={"/"} className="mx-3">
+          {/* <Link href={"/"} className="mx-3">
             <AiFillInstagram className="hover:text-slate-600" />
-          </Link>
-          <Link href={"/"}>
+          </Link> */}
+          {/* <Link href={"/"}>
             <BsTwitter className="hover:text-slate-600" />
-          </Link>
+          </Link> */}
         </div>
         <div className="text-slate-400 flex justify-center items-center md:mt-1">
           <span className="text-slate-300 text-sm ml-2">میزبانی شده توسط</span>
 
           <Link
             className="flex font-bold justify-center items-center hover:text-slate-600"
-            href={"/"}
+            href={statics.sponserLink}
           >
-            لیارا <Box1 size="16" className="mr-2" />
+            {statics.sponserName} <Box1 size="16" className="mr-2" />
           </Link>
         </div>
         <div className="text-slate-400 flex justify-center items-center md:text-[14px]">
@@ -41,7 +45,10 @@ const Footer = () => {
             طراحی و توسعه توسط
           </span>
           <div style={{ fontFamily: "cursive" }}>
-            <Link className={"font-bold hover:text-slate-600"} href={"/"}>
+            <Link
+              className={"font-bold hover:text-slate-600"}
+              href={statics.sahDev}
+            >
               Sah
             </Link>
           </div>
@@ -50,7 +57,10 @@ const Footer = () => {
             - و -{" "}
           </span>
           <div style={{ fontFamily: "cursive" }}>
-            <Link className={"font-bold hover:text-slate-600"} href={"/"}>
+            <Link
+              className={"font-bold hover:text-slate-600"}
+              href={statics.zolfuDev}
+            >
               Zolfu
             </Link>
           </div>
